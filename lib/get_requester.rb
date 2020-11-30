948114b159a19e50aa62cfeca1527b51e6db9553
Your Username <you@example.com>
2020-11-29 19:42:25 -0500

    commit tree 52d1a01614fb7e55050d25287af2fcb38ce5787e

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,20 @@
-# Write your code here
+require 'open-uri'
+require 'net/http' 
+require 'json'
+
+class GetRequester 
+attr_accessor :url
+
+    def initialize(url)
+        @url = url
+    end 
+
+    def get_response_body 
+        uri = URI.parse(url)
+        uri.open.string
+    end  
+
+    def parse_json 
+        JSON.parse(get_response_body)
+    end 
+end 
